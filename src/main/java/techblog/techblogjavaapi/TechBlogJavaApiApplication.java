d0309ec591de9753533e9a9bbd915855b21483d8
Stefanie <stefaniepires91@gmail.com>
2022-03-04 21:45:53 -0600

    first commit

---
+++ src/main/java/techblog/techblogjavaapi/TechBlogJavaApiApplication.java
@@ -0,0 +1,13 @@
+package techblog.techblogjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBlogJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBlogJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techblog/techblogjavaapi/TechBlogJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package techblog.techblogjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
